929fb2aa95b19a9d902cb4e2dfc60e13edc9ea64
Tarak Gupta Drebes <tarak.guptadrebes@gmail.com>
2026-07-05 19:39:00 +0100

    remove redundant code in app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import os
-import sys
-
-src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "src"))
-if src_path not in sys.path:
-    sys.path.insert(0, src_path)
-
 import pandas as pd
 import numpy as np
 import streamlit as st
